0b304fa8cb5af6e90e9044ee88ef8e722f51f3ff
aneza2023 <163205385+aneza2023@users.noreply.github.com>
2024-11-04 14:35:33 +0000

    linked list created

--- push_swap.c
+++ push_swap.c
@@ -6,21 +6,32 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 10:20:10 by codespace         #+#    #+#             */
-/*   Updated: 2024/11/04 13:50:24 by codespace        ###   ########.fr       */
+/*   Updated: 2024/11/04 14:35:04 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdlib.h>
 
+t_node *inserting_arg(t_node *node, int arg)
+{
+	t_node *new_node;
+
+	new_node = (t_node*)malloc(sizeof(t_node));
+	if (new_node == NULL){
+		printf("Allocation failed");
+		return (node);
+	}
+	new_node->value = arg;
+	new_node->next = node;
+	return (new_node);
+}
 
 int	main(int argc, char *argv[])
 {
 	int i;
 	int arg;
 	t_node *node;
-	t_node *new_node;
-	t_node *current;
 	
 	if (argc <= 1){
 		printf("Numbers for swap_push were not provided\n");
@@ -30,27 +41,13 @@ int	main(int argc, char *argv[])
 	node = NULL;
 	while (i != argc){
 		arg = atoi(argv[i]);
-		new_node = (t_node *)malloc(sizeof(t_node));
-		if (new_node == NULL){
-			printf("Allocation failed");
-			return (NULL);
-		}
-		new_node->value = arg;
-		new_node->next = NULL;
-		if (node == NULL)
-			node = new_node;
-		else {
-			current = node;
-			while (current->next != NULL)
-				current = current->next;
-			current->next = new_node;
-		}
+		node = inserting_arg(node, arg);
+		i++;	
 	}
-	while (current != NULL){
-		printf("%d", current->value);
-		current = current -> next;
-	}
+/* 	while (node != NULL){
+		printf("%d ->", node->value);
+		node = node ->next;
+	} */
 	return (0);
 }
-
 // {}

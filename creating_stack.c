bba8624700c7b4ec4b8441a4d02addd1c587b322
aneza2023 <163205385+aneza2023@users.noreply.github.com>
2024-11-04 14:39:59 +0000

    separeting stack creation

--- creating_stack.c
+++ creating_stack.c
@@ -1,38 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   creating_stack.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/04 10:20:10 by codespace         #+#    #+#             */
-/*   Updated: 2024/11/04 14:38:02 by codespace        ###   ########.fr       */
+/*   Created: 2024/11/04 14:36:36 by codespace         #+#    #+#             */
+/*   Updated: 2024/11/04 14:39:12 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char *argv[])
+t_node *inserting_arg(t_node *node, int arg)
 {
-	int i;
-	int arg;
-	t_node *node;
-	
-	if (argc <= 1){
-		printf("Numbers for swap_push were not provided\n");
-		return (1);
+	t_node *new_node;
+
+	new_node = (t_node*)malloc(sizeof(t_node));
+	if (new_node == NULL){
+		printf("Allocation failed");
+		return (node);
 	}
-	i = 1;
-	node = NULL;
-	while (i != argc){
-		arg = atoi(argv[i]);
-		node = inserting_arg(node, arg);
-		i++;	
-	}
-/* 	while (node != NULL){
-		printf("%d ->", node->value);
-		node = node ->next;
-	} */
-	return (0);
+	new_node->value = arg;
+	new_node->next = node;
+	return (new_node);
 }
-// {}
